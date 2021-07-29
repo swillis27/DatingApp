1005c8189db38ef305202ed0493979bd71aba55f
swillis <swillis@etain-software.com>
2021-07-29 17:05:33 +0100

    End of Section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,12 @@ namespace API
             services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
+            services.AddSwaggerGen(c => { 
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "API",
+                    Version = "v1"
+                    });
+            });   
             services.AddIdentityServices(_config);
         }
 
